0d9890da6a94d6045ca173ac4e0e46a18f192056
Leo Thorp <leo.thorp1@gmail.com>
2017-10-14 21:44:37 -0500

    formatting

--- NeuralNetwork/NetworkModels/Neuron.js
+++ NeuralNetwork/NetworkModels/Neuron.js
@@ -3,14 +3,15 @@ const Network = require('./Network');
 const Synapse = require('./Synapse');
 const Sigmoid = require('./Sigmoid');
 
-/* Properties 
-public Guid Id { get; set; }
-public List<Synapse> InputSynapses { get; set; }
-public List<Synapse> OutputSynapses { get; set; }
-public double Bias { get; set; }
-public double BiasDelta { get; set; }
-public double Gradient { get; set; }
-public double Value { get; set; }
+/* 
+    Neuron Class Properties: 
+    public Guid Id { get; set; }
+    public List<Synapse> InputSynapses { get; set; }
+    public List<Synapse> OutputSynapses { get; set; }
+    public double Bias { get; set; }
+    public double BiasDelta { get; set; }
+    public double Gradient { get; set; }
+    public double Value { get; set; }
 */
 
 const sum = (arr, transform = x => x) => arr.reduce((acc, curr) => {
@@ -28,7 +29,6 @@ class Neuron {
         this.BiasDelta = 0;
         this.Gradient = 0;
         this.Value = 0;
-
 
         if (inputNeurons != null) {
             inputNeurons.forEach(inputNeuron => {
